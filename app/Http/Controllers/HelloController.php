b02fd1580b7b04f91fe6ff749d0793bb8e33e5b2
tomozo <work.mail7530@gmail.com>
2025-05-06 11:06:01 +0900

    updatte: ミドルウェアのルーティング

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+    public function index(Request $request)
+    {
+        $data = [
+            'msg' => $request->hello,
+        ];
+
+        return view('chpter1.other', $data);
+    }
+
+    public function other(Request $request)
+    {
+        $data = [
+            'msg' => $request->bye
+        ];
+
+        return view('chpter1.other', $data);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\indexController;
+use App\Http\Controllers\HelloController;
+use App\Http\Middleware\HelloMiddleware;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -10,3 +12,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [indexController::class, 'index'])->name('Hello');
 Route::get('/hello', [indexController::class, 'hello']);
 
+Route::get('/where/{id}', [indexController::class, 'where'])->where('id', '[0-9]+');
+
+// ルートグループをミドルウェアで割り当てる
+Route::middleware([HelloMiddleware::class])->group(function () {
+    Route::get('/hello', [HelloController::class, 'index']);
+    Route::get('/hello/other', [HelloController::class, 'other']);
+});
