7345735156572ab59f0e374f876176e5ee9dfd15
tomozo <work.mail7530@gmail.com>
2025-05-06 11:40:00 +0900

    update: 名前空間によるルーティング

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\indexController;
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\SampleController;
 use App\Http\Middleware\HelloMiddleware;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,10 @@ Route::get('/where/{id}', [indexController::class, 'where'])->where('id', '[0-9]
 Route::middleware([HelloMiddleware::class])->group(function () {
     Route::get('/hello', [HelloController::class, 'index']);
     Route::get('/hello/other', [HelloController::class, 'other']);
+});
+
+// 名前空間とクループルート
+Route::namespace('Sample')->group(function() {
+    Route::get('/sample', [SampleController::class, 'index']);
+    Route::get('/sample/other', [SampleController::class, 'other']);
 });
